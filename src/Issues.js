8e9820639283885adcd93092bba295976ad3273b
David Tang <dtang@auditboard.com>
2020-09-19 01:08:15 -0700

    3. Filtering issues by label

--- src/Issues.js
+++ src/Issues.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import IssueList from "./IssueList";
 import { Link } from "react-router-dom";
 
 export default function Issues({ issues, labels, labelsById }) {
+  const [selectedLabelId, setSelectedLabelId] = useState();
+  const [filteredIssues, setFilteredIssues] = useState(issues);
+
+  const options = [{ id: "-", name: "All" }].concat(labels);
+
+  function handleLabelChange(event) {
+    const isAllSelected = event.target.value === "-";
+    if (isAllSelected) {
+      setFilteredIssues(issues);
+    } else {
+      const selectedLabelId = Number(event.target.value);
+      const filteredIssues = issues.filter((issue) => {
+        return issue.label === selectedLabelId;
+      });
+
+      setFilteredIssues(filteredIssues);
+    }
+
+    setSelectedLabelId(selectedLabelId);
+  }
+
   return (
     <>
       <div className="text-right mb-3">
@@ -10,7 +31,28 @@ export default function Issues({ issues, labels, labelsById }) {
           Create Issue
         </Link>
       </div>
-      <IssueList issues={issues} labels={labels} labelsById={labelsById} />
+
+      <div className="d-flex justify-content-end mb-3">
+        <select
+          className="form-control w-25"
+          value={selectedLabelId}
+          onChange={handleLabelChange}
+        >
+          {options.map((label) => {
+            return (
+              <option key={label.id} value={label.id}>
+                {label.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+
+      <IssueList
+        issues={filteredIssues}
+        labels={labels}
+        labelsById={labelsById}
+      />
     </>
   );
 }
