05ca76408f6d71758b3c2b08b7173db365cb748d
David Tang <dtang@auditboard.com>
2020-10-22 17:38:48 -0700

    3. useContext instead of LabelsContext.Consumer

--- src/IssueForm.js
+++ src/IssueForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { LabelsContext } from "./contexts";
 
 export default function IssueForm({
@@ -7,6 +7,7 @@ export default function IssueForm({
 }) {
   const [title, setTitle] = useState(issue.title);
   const [labelId, setLabelId] = useState(issue.label);
+  const labels = useContext(LabelsContext);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -27,43 +28,37 @@ export default function IssueForm({
   }
 
   return (
-    <LabelsContext.Consumer>
-      {(labels) => {
-        return (
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="title">Title</label>
-              <input
-                type="text"
-                value={title}
-                onChange={handleTitleChange}
-                className="form-control"
-                id="title"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="label">Label</label>
-              <select
-                className="form-control"
-                id="label"
-                value={labelId}
-                onChange={handleLabelChange}
-              >
-                {labels.map((label) => {
-                  return (
-                    <option key={label.id} value={label.id}>
-                      {label.name}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Save
-            </button>
-          </form>
-        );
-      }}
-    </LabelsContext.Consumer>
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          value={title}
+          onChange={handleTitleChange}
+          className="form-control"
+          id="title"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="label">Label</label>
+        <select
+          className="form-control"
+          id="label"
+          value={labelId}
+          onChange={handleLabelChange}
+        >
+          {labels.map((label) => {
+            return (
+              <option key={label.id} value={label.id}>
+                {label.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Save
+      </button>
+    </form>
   );
 }
